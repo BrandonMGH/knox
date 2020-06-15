2cc1d69a638689abbbc5a708d2d2a385007e0a11
BrandonMGH <BrandonMGH@Yahoo.com>
2020-06-14 22:27:26 -0700

    update

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,17 @@
-import React, { useEffect} from 'react'
+import React, { useState } from 'react';
 
 const Main = () => {
+   
+    const mongoDBPost = () => {
+        const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+
+    }
     
     return (
         <>
         <h1>Main</h1>
         <input></input>
+        <button onClick={mongoDBPost}>MongoDB post</button>
         </>
     )
 }
