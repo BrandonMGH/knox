8cc72e66972315b05692e0162fd7361699c2910f
BrandonMGH <BrandonMGH@Yahoo.com>
2020-10-11 13:21:55 -0700

    working on conditional router logic

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-ro
 import LoginPage from "../LoginPage/LoginPage.js";
 import Test from "../Test/Test.js"
 
+let loggedIn = true
+
 const Main = () => {
   return (
     <Router>
-       <Route path="/Login" exact component={LoginPage} />
-       <Route path="/test" exact component={Test} />
-       <Route path="*" exact component={LoginPage} />
+       <Router path="*" exact component={LoginPage} />
+       {loggedIn ? <Test/> : <LoginPage />}
+       <Router />
     </Router>
   );
 };
